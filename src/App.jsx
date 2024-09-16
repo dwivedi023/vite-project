68b8633d807a1d15a558be27781348ede54e6f0c
Adarsh Dwivedi <dwivediadarsh074@gmail.com>
2024-09-16 11:03:00 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div className="m-10 p-10  text-5xl  bg-black">
+      <h1 className="text-bold text-red-400"  >hello dosto suna hai ap hame bhool gaye hain </h1>
+    </div>
+  )
+}
+
+export default App
